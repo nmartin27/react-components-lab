114672780d8a4f12840fc8ab8b55379f1e6f4861
nmartin <nate.martin27@gmail.com>
2024-12-03 18:43:17 -0500

    images and alts

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,11 +1,11 @@
 import "./WeatherForecast.css";
 
 const DayWeather = (props) => {
-  const { day, conditions, time } = props;
+  const { day, conditions, time, img, imgAlt } = props;
   return (
     <div className="weather">
       <h2>{day}</h2>
-      <img src="" alt="" />
+      <img src={img} alt={imgAlt} />
       <p>
         <span>Conditions:  </span>{conditions}
       </p>
